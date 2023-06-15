2a663cb1f2d644b82959f98da99fc3cc3f8c43e6
mfeldmay <136701307+mfeldmay@users.noreply.github.com>
2023-06-15 15:34:37 +0200

    fixed build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,15 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
+  kotlin {
+    jvmToolchain(17)
+  }
+
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
   buildFeatures {
     compose = true
@@ -48,7 +52,7 @@ android {
 
 dependencies {
 
-  implementation("androidx.core:core-ktx:1.9.0")
+  implementation("androidx.core:core-ktx:1.10.1")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
   implementation("androidx.activity:activity-compose:1.7.2")
   implementation(platform("androidx.compose:compose-bom:2023.03.00"))
